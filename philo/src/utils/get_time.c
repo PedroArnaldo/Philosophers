6747ff829364aa0207f5e8b10cb57465ecdb96fc
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-01-18 09:37:01 -0300

    created get time

--- philo/src/utils/get_time.c
+++ philo/src/utils/get_time.c
@@ -1,13 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_times.c                                        :+:      :+:    :+:   */
+/*   get_time.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: parnaldo <parnaldo@student.42.rio>         +#+  +:+       +#+        */
+/*   By: parnaldo <parnaldo@student.42.rio >        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 18:19:37 by parnaldo          #+#    #+#             */
-/*   Updated: 2023/01/17 18:20:36 by parnaldo         ###   ########.fr       */
+/*   Updated: 2023/01/18 06:16:37 by parnaldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include "../../includes/philo.h"
+
+long    time_now(void)
+{
+
+    struct timeval tv;
+    long    mili;
+    
+    gettimeofday(&tv, NULL);
+    mili = tv.tv_sec * 1000;
+    mili = tv.tv_usec / 1000;
+    return (mili);
+}
